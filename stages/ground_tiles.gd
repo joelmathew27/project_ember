4137b387eee472438aa40a383bd66b2c297137af
Joel Mathew <joelmathew27@gmail.com>
2025-04-17 16:04:01 -0500

    ground tiles features

---
+++ stages/ground_tiles.gd
@@ -0,0 +1,32 @@
+extends TileMapLayer
+
+# when mouse hover, change tile to atlas coords (0, 1) 
+# and when left cell, change back to previous tile
+
+var previousTile = Vector2i(0, 0)
+var previousTileID = Vector2i(1, 1)
+const TILE_SOURCE_ID = 0
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		var mouse_pos = get_global_mouse_position()
+		var tile_pos = local_to_map(mouse_pos)
+		if (has_tile(mouse_pos)) && tile_pos != previousTile:
+			set_cell(previousTile, TILE_SOURCE_ID, previousTileID)
+			previousTile = tile_pos
+			previousTileID = get_cell_atlas_coords(tile_pos)
+			set_cell(tile_pos , 0, Vector2i(0, 1))
+
+
+func has_tile(pos):
+	if get_cell_source_id(pos) != -1:
+		print(get_cell_source_id(pos), " - source id")
+		print(get_cell_atlas_coords(pos), " - atlas id")
+		return true
+	else:
+		print(get_cell_source_id(pos), " - source id")
+		print(get_cell_atlas_coords(pos), " - atlas id")
+		return true
+
+
+	
